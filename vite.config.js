bb8b98931dc439e285b88d45c3b5a8630522bf05
Roberto <rfernandez5190@esdmadrid.es>
2025-03-21 20:37:26 +0100

    add vitest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true, // Habilitar variables globales para pruebas (si las necesitas)
+    environment: 'jsdom', // Para pruebas de DOM
+  },
+});
