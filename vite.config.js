76f2d41b5e56efdee44732062c28e36e61ebdf2f
Roberto <rfernandez5190@esdmadrid.es>
2025-03-21 20:53:39 +0100

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import viteStaticCopy from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true, // Habilitar variables globales para pruebas (si las necesitas)
-    environment: 'jsdom', // Para pruebas de DOM
-  },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/assets/*',
+          dest: 'dist/assets/',
+        },
+      ],
+    }),
+  ],
 });
