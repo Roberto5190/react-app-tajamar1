cb7231d5fedf22395a9b68dec93039faa9f08ebb
Roberto <rfernandez5190@esdmadrid.es>
2025-03-25 18:09:00 +0100

    added viteconfig output

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Asegúrate de que sea 'dist'
+  },
   test: {
     globals: true, 
     enviroment: "jsdom",
