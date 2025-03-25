f7dd50bcf44e7e07363300bc8b90317cf5223b5b
Roberto <rfernandez5190@esdmadrid.es>
2025-03-25 18:52:36 +0100

    fixed app test file

--- App.test.jsx
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from "vitest"
-import { render, screen } from "@testing-library/react"
-
-describe("App Component", () => {
-    it("Debe renderizar el texto 'Destinos'", () => {
-        render(<App />);
-    })
-})
---
+++ src/App.test.jsx
@@ -0,0 +1,13 @@
+import { describe, it, expect } from "vitest"
+import { render, screen } from "@testing-library/react"
+import App from "./App"
+
+describe("App Component", () => {
+    it("Debe renderizar el texto 'Click on the Vite'", () => {
+        // Renderiza el componente App
+        render(<App />);
+        
+        
+        expect(screen.getByText(/Click on the Vite/i)).toBeInTheDocument();
+    });
+})
