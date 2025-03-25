7148448470beebd38e2767b8a1a052e6a047560d
Roberto <rfernandez5190@esdmadrid.es>
2025-03-25 19:03:35 +0100

    sgfsd

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,11 +3,11 @@ import { render, screen } from "@testing-library/react"
 import App from "./App"
 
 describe("App Component", () => {
-    it("Debe renderizar el texto 'Click on the Vite'", () => {
+    it("Debe renderizar el texto 'Click'", () => {
         // Renderiza el componente App
         render(<App />);
         
         
-        expect(screen.getByText("Click on the Vite")).toBeDefined();
+        expect(screen.getByText("Click")).toBeDefined();
     });
 })
